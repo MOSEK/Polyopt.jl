c56be88626be301dee7e081077bbd0b2a96fa194
Joachim Dahl <dahl.joachim@gmail.com>
2015-02-04 08:49:10 +0100

    Frank's counter example

--- test/sparsity.jl
+++ test/sparsity.jl
@@ -98,40 +98,7 @@ prob = momentprob(1, f, [g3,g4])
 X, t, y, solsta = solve_mosek(prob)
 end
 
-
 if false
-    # Haverly1 standard pooling instance
-    x14, x24, x35, x36, x45, x46, w4 = variables(["x_{14}", "x_{24}", "x_{35}", "x_{36}", "x_{45}", "x_{46}", "w_4"])
-
-    c14, c24, c35, c36, c45, c46 = [6, 16, 1, -9, -5, -15]
-    q1, q2, q3, q5, q6 = [ 3, 1, 2, 5//2, 3//2 ]
-    ub1, ub2, ub3, ub4, ub5, ub6 = [ 300, 300, 300, 300, 100, 200 ] // 300
-
-    flow_eq    = [ x14+x24-(x45+x46) ]
-    blend_eq   = [ w4*(x45+x46)-(q1*x14+q2*x24) ]
-    qual_bnd   = [ q5*(x45+x36)-(w4*x45+q3*x35), q6*(x46+x36)-(w4*x46+q3*x36) ]
-    cap_bnd    = [ ub1-x14, ub2-x24, ub3-(x35+x36), ub4-(x45+x46), ub5-(x35+x45), ub6-(x36+x46)]
-    flow_bnd   = [ x14,x24,x35,x36,x45,x46]
-
-    obj = dot([c14, c24, c35, c36, c45, c46], [x14, x24, x35, x36, x45, x46])
-    prob = momentprob(order, obj, [qual_bnd, cap_bnd, flow_bnd], [flow_eq,blend_eq])
-
-    X, t, y, solsta = solve_mosek(prob)
-
-    Ao = sparsity(1, obj)
-    Ai = sum([sparsity(1,v) for v=[obj, qual_bnd, cap_bnd, flow_bnd, flow_eq, blend_eq]])
-
-    println("Sparsity of used monominals:\n", full(Ao + Ai))
-
-    cliques = chordal_embedding(Ao + Ai)
-    println("cliques for sparsity:", cliques)
-
-    prob2 = momentprob_chordal_new(1, cliques, obj, [qual_bnd, cap_bnd, flow_bnd], [flow_eq,blend_eq])
-
-    X2, t2, y2, solsta2 = solve_mosek(prob2)
-end
-
-if true
 
 x1, x2, x3 = variables(["x1", "x2", "x3"])
 
@@ -150,6 +117,38 @@ A = sparsity(f.deg >> 1, f)
 println("Sparsity of used monominals:\n", full(A))
 
 cliques = chordal_embedding_new(A)
+println("cliques for sparsity:", cliques)
+
+prob2 = momentprob_chordal_new(f.deg >> 1, cliques, f)
+
+X2, t2, y2, solsta2 = solve_mosek(prob2)
+
+end
+
+
+# Frank's counter example
+if true
+
+x, = variables(["x"])
+
+f = x^6 + x + 100
+
+prob = momentprob(f.deg >> 1, f)
+X, t, y, solsta = solve_mosek(prob)
+
+v = monomials(f.deg >> 1,[x])
+@test approxzero( f - t -dot(v,X[1]*v) )
+
+println("f: ", f)
+
+println("full moment-matrix:\n", v*v')
+#A = sparsity(f.deg >> 1, f)
+A = sparse([1 1 1 1; 1 1 1 1; 1 1 1 0; 1 1 0 1])
+println("Sparsity of used monominals:\n", full(A))
+
+
+cliques = chordal_embedding_new(A)
+
 println("cliques for sparsity:", cliques)
 
 prob2 = momentprob_chordal_new(f.deg >> 1, cliques, f)
